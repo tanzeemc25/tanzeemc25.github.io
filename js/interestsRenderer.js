1c89e8bef5f1d1600dfbcd2c897f6d682d1bcea0
Tanzeem Chowdhury <tanzeem.c25@gmail.com>
2020-07-25 06:37:31 -0400

    FEAT | Inerests page

---
+++ js/interestsRenderer.js
@@ -0,0 +1,65 @@
+$(function() {
+
+	$.each(filters, function( i, v ) {
+    	appendToFilters(v);
+	});
+
+    $.each(posters, function( i, v ) {
+    	appendToGrid(v);
+	});
+
+	var $grid = $('.grid').isotope({
+		itemSelector: '.poster'
+	});
+
+	$('input:checkbox').change(function() {
+
+		var isoFilter = "";
+
+		$( "input:checkbox" ).each(function() {
+			if(this.checked) {
+				isoFilter += "." + $(this).val();
+	        }
+		});
+
+		$grid.isotope({ filter: isoFilter });
+    });	
+
+});
+
+
+function appendToGrid(v) {
+
+	var fileName = v["file"];
+	var filters = v["filters"];
+	var link = v["link"] == "" ? "javascript:void(0)" : v["link"];
+	var lbl = v["label"]
+
+	var posterDiv = 
+		'<div class="poster ' + filters + '">' + 
+			'<img src="../img/interests/' + fileName + '" class="posterImg" height="200" width="200">' +  
+			'<div class="middle">' +
+				'<div class="text">' + 
+					'<a href="' + link + '" target="_blank">' + lbl + '</a>' + 
+				'</div>' +
+			'</div>' +
+      	'</div>'
+	;
+
+	$("#poster-grid").append(posterDiv);
+}
+
+
+function appendToFilters(v) {
+
+	var filter = v["filter"];
+	var text = v["text"];
+
+	var filterCheckBox = 
+		'<label class="checkbox-inline">' + 
+			'<input type="checkbox" name="filter" value="' + filter + '"> ' + text + 
+		'</label>'
+	;
+
+	$("#filtersMain").append(filterCheckBox);
+}
