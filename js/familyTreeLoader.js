208745a5241f336ce2498f77babfac64d5b161a4
Tanzeem Chowdhury <tanzeem.c25@gmail.com>
2023-12-31 10:11:36 -0500

    family tree

--- js/familyTreeLoader.js
+++ js/familyTreeLoader.js
@@ -1,5 +1,6 @@
 import db from '../js/data/peopleData.json' assert { type: 'json' };
 
+// Fix or prep data
 for (const element of db) {
 
     // Split partners - Because the shitty Google-Sheet-to-JSON export stringifies arrays
@@ -12,9 +13,16 @@ for (const element of db) {
     if (http.status == 404) element["img"] = "../assets/img/person/default.jpg";
 }
 
+// Determine tree root from URL param
+const params = new Proxy(new URLSearchParams(window.location.search), {
+    get: (searchParams, prop) => searchParams.get(prop),
+});
+let treeRoot = params.root !== null ? [params.root] : [];
+
+// Build tree
 let family = new FamilyTree(document.getElementById("tree"), {
     orderBy: "sibOrder",
-    roots: [95],
+    roots: treeRoot,
     showXScroll: true,
     showYScroll: true,
     siblingSeparation: 40,
